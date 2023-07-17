f346a3c8fb433e9671db80229eccadf5c3c6b35b
小西侑樹 <77260263+YukiKonishi@users.noreply.github.com>
2023-07-17 15:18:39 +0900

    Update crssy.rb

--- Formula/crssy.rb
+++ Formula/crssy.rb
@@ -4,13 +4,14 @@ class Crssy < Formula
     homepage "https://yukikonishi.github.io/crssy/"
     url "https://github.com/YukiKonishi/crssy/releases/download/v0.1.16/crssy-0.1.16_darwin_amd64.tar.gz"
     version VERSION
-    sha256 "308046086a8531f162b3096002951adb629c55a6c99e7d335f7f60732ff93a67"
+    sha256 "b67ba15c914a84eda36cb93fdd1de064e3d0dfa33de127da93400bc784ef23c4"
     license "MIT"
     option "without-completions", "Disable bash completions"
     depends_on "bash-completion@2" => :optional
 def install
     bin.install "crssy"
-    bash_completion.install "completions/bash/crssy" if build.with? "completions" end
+    #bash_completion.install "completions/bash/crssy" if build.with? "completions" 
+  end
   test do
     system bin/"crssy", "--version"
     end
