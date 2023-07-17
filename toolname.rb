328fc8ef255eb127d8d2fa0e09100913b1d555ac
小西侑樹 <77260263+YukiKonishi@users.noreply.github.com>
2023-07-17 15:00:53 +0900

    Add files via upload

---
+++ toolname.rb
@@ -0,0 +1,17 @@
+VERSION="1.1.16"
+class Crssy < Formula
+    desc ""
+    homepage "https://yukikonishi.github.io/crssy/"
+    url "https://github.com/YukiKonishi/crssy/releases/download/v#{VERSION}/crssy-#{VERSION}_darwin_amd64.tar.gz"
+    version VERSION
+    sha256 "308046086a8531f162b3096002951adb629c55a6c99e7d335f7f60732ff93a67"
+    license "MIT"
+    option "without-completions", "Disable bash completions"
+    depends_on "bash-completion@2" => :optional
+def install
+    bin.install "crssy"
+    bash_completion.install "completions/bash/crssy" if build.with? "completions" end
+  test do
+    system bin/"crssy", "--version"
+    end
+end
